9add96f8c335d9f411fb9c833b0743a3fd6f96de
michaell.mahan <mmahan13@gmail.com>
2021-12-03 00:06:29 +0100

    ORDERS new view order harcode

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+  public orderHeader:any = []
 
   constructor() {}
+
+  ngOnInit(){
+     this.orderHeader = [{
+      name:"Frutos secos Yoli",
+      number_orders:"1",
+      created_at:"21/11/2021",
+      updated_at:"02/12/2021",
+    },
+    {
+      name:"Sabor a Miel",
+      number_orders:"2",
+      created_at:"21/11/2021",
+      updated_at:"02/12/2021",
+    },
+    {
+      name:"Natural food",
+      number_orders:"3",
+      created_at:"21/11/2021",
+      updated_at:"02/12/2021",
+    }];
+  }
+  
 
 }
